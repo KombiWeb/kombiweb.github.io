8cd6d2f3298913e38c3b8d1fbf1d09ca4437d73c
Tiago Debastiani <debastiansiter@gmail.com>
2015-04-23 14:21:11 -0300

    Ajust Gamepad Connection
    - Add the function to gamepad connect/disconnect by pressing the 'Start Button'
    - Changed the Materialize Toast Notifications of connections

--- js/gamepad.js
+++ js/gamepad.js
@@ -1,6 +1,8 @@
 var hasGP = false,
 	showBut,
-	repGP;
+	repGP,
+	connectTrigger
+	gpConnected = false;
 
 function canGame() {
 	return "getGamepads" in navigator;
@@ -28,52 +30,69 @@ function reportOnGamepad() {
 	$("#gamepadDisplay").html(html);
 }
 */
-
-function buttonPressed(b) {
-	if (typeof(b) == "object") {
-		return b.pressed;
-	}
-	return b == 1.0;
-}
-
 $(document).ready(function() {
 	
 	if(canGame()) {
 
+
 		$(window).on("gamepadconnected", function() {
 			hasGP = true;
-			gpConnected = true;
-			Materialize.toast('Controller Conected', 3000,'rounded',function(){
-				Materialize.toast('ID: '+navigator.getGamepads()[0].id, 3500, 'rounded');
-			});
-			$('.controller').fadeTo('slow', 1, function() {
-				$(this).css({display: 'block'})
-			});
-			console.log("connection event");
-			//repGP = window.setInterval(reportOnGamepad,100);
-			showBut = window.setInterval(buttonsEvent,100);
+			console.log("You have some Gamepads connected! \nPress 'Start Button' to connect the first!");
+			connectTrigger = window.setInterval(function() {
+				if (navigator.getGamepads()[0].buttons[15].pressed){
+					if(gpConnected)
+						disconnection();
+					else
+						connection();
+				}
+			},500);
 		});
 
 		$(window).on("gamepaddisconnected", function() {
-			gpConnected = false;
 			console.log("disconnection event");
-			Materialize.toast('Controller Disconected', 3000,'rounded');
-			$(".controller").hide();
-			//window.clearInterval(repGP);
-			window.clearInterval(showBut);
+			hasGP = false;
+			window.clearInterval(connectTrigger);
 		});
 
 		//setup an interval for Chrome
+		
 		var checkGP = window.setInterval(function() {
-			console.log('checkGP');
+			//console.log('checkGP');
 			if(navigator.getGamepads()[0]) {
 				if(!hasGP) $(window).trigger("gamepadconnected");
 				window.clearInterval(checkGP);
 			}
 		}, 500);		
+
 	}
+
 });
 
+function connection() {
+	gpConnected = true;
+	Materialize.toast(navigator.getGamepads()[0].id.slice(0, navigator.getGamepads()[0].id.indexOf('(')-1)+" Connected", 3000,'rounded',function(){});
+	$('.controller').fadeTo('slow', 1, function() {
+		$(this).css({display: 'block'})
+	});
+	console.log("Gamepad: "+navigator.getGamepads()[0].id+"\nPress 'Start Button' to disconnect!");
+;
+	//repGP = window.setInterval(reportOnGamepad,100);
+
+	showBut = window.setInterval(buttonsEvent,100);
+}
+
+function disconnection() {
+	gpConnected = false;
+	console.log("Gamepad Disconnected");
+	Materialize.toast(navigator.getGamepads()[0].id.slice(0, navigator.getGamepads()[0].id.indexOf('(')-1)+' Disconected', 2500,'rounded');
+	$(".controller").fadeTo('fast',0, function(){
+		$(this).hide();
+	});
+
+	//window.clearInterval(repGP);
+
+	window.clearInterval(showBut);
+}
 
 function buttonsEvent() {
 	var gp = navigator.getGamepads()[0];
@@ -104,7 +123,8 @@ function buttonsEvent() {
 			}
 
 			if (gp.buttons[1].pressed) { // Button U
-				window.location.href = "http://whothey.github.io";
+				$('#btucra').trigger('onClick');
+				//window.location.href = "http://whothey.github.io";
 			}
 
 			if (gp.buttons[2].pressed) { // Button Y
@@ -123,18 +143,20 @@ function buttonsEvent() {
 	}else {
 		$(".controller-btn").hide('fast');
 		$(".controller-menu").hide('fast');
-	}
+	}	
 
 	if (gp.axes[4] != 0) {  // Right Stick Y-axis
 		window.scrollBy(0,gp.axes[4]*50);
 		if (gp.axes[4] > 0) {
+			$("#stickR-up").hide();
 			$("#stickR-down").show();
 		}
 		else {
+			$("#stickR-down").hide();
 			$("#stickR-up").show();
 		}
 	} 
-	else if (gp.axes[4] == 0) {
+	else  {
 		$("#stickR-down").hide();
 		$("#stickR-up").hide();
 	}
@@ -142,13 +164,15 @@ function buttonsEvent() {
 	if (gp.axes[3] != 0) {  //Right Stick X-axis
 		window.scrollBy(gp.axes[3]*50,0);
 		if (gp.axes[3] > 0) {
+			$("#stickR-left").hide();
 			$("#stickR-right").show();
 		}
 		else {
+			$("#stickR-right").hide();
 			$("#stickR-left").show();
 		}
 	} 
-	else if (gp.axes[4] == 0) {
+	else  {
 		$("#stickR-right").hide();
 		$("#stickR-left").hide();
 	}
